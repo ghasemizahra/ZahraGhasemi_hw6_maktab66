2eeed1ab1b74309419f9e9719ffe34bbb045dc36
Zahra <z.ghasemi.02018@gmail.com>
2021-12-15 22:30:41 +0330

    update

--- 1/common.js
+++ 1/common.js
@@ -19,20 +19,20 @@
 //   console.log(getSameCount("platformforgeeks", "geeksforgeeks"));
 ////////////////////////////////////////////////////////////////////////////
 
-// function countMatch(s1,s2){
-//     var count = 0;
-//     while(s1.length && s2.length){
-//         if(s2.includes(s1.charAt(0))){
-//             count++;
-//             s2 = s2.replace(s1.charAt(0),"");
-//             s1 = s1.slice(1);
-//         }
-//         else {
-//             s1 = s1.slice(1);
-//         }
-//     }
-//     return count;
-// }
+function countMatch(s1,s2){
+    var count = 0;
+    while(s1.length && s2.length){
+        if(s2.includes(s1.charAt(0))){
+            count++;
+            s2 = s2.replace(s1.charAt(0),"");
+            s1 = s1.slice(1);
+        }
+        else {
+            s1 = s1.slice(1);
+        }
+    }
+    return count;
+}
 
-// console.log(countMatch("aabcc", "adcaa"));
-////////////////////////////////////////////////////////
+console.log(countMatch("aabcc", "adcaaffaaad"));
+//////////////////////////////////////////////////////
